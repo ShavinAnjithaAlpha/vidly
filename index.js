d5fcdd1fa81d64644521991483c819cddab3bd7e
Shavin Anjitha <shavinanjithachandrawansha@gmail.com>
2023-10-15 10:45:55 +0530

    Add mongo connection variables to the config file and set the heroku env variables.

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require("express");
 const winston = require("winston");
 
 const app = express();
+require("./start/config")(); // create the config
 require("./start/validation")(); // create the validation
 require("./start/routes")(app); // create the routes
 require("./start/db")(); // create the mongo database connection
 require("./start/logging")(); // create the logger
-require("./start/config")(); // create the config
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
--- start/db.js
+++ start/db.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 const winston = require("winston");
+const config = require("config");
 
 module.exports = function () {
   // connect to the database
-  mongoose.connect("mongodb://127.0.0.1/vidly").then(() => {
+  mongoose.connect(config.get("db")).then(() => {
     winston.info("Connected to MongoDB...");
   });
 };
